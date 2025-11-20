be7880a111b6ce2810fb43c970e09e174d907a2f
Moussa Montassar <moussamontassar.pro@gmail.com>
2025-11-20 17:25:44 +0100

    fini et a moité encore compris

--- ex_04.php
+++ ex_04.php
@@ -7,5 +7,5 @@ while ($nombre <= 10) {
     echo $nombre . "\n";
     
     $nombre = $nombre + 2; 
-}
+    }
 ?>
---
+++ ex_08.php
@@ -0,0 +1,9 @@
+<?php
+
+
+    $id = array("surname" => "Dupont", "name" => "Sophie", "age" => 27);
+
+   foreach ($id as $key => $value) {
+
+       echo $key . " cc " . $value . "\n"; 
+   }
