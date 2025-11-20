090af4394c9ff798c6d5551eb1fb648381ccabbe
Moussa Montassar <moussamontassar.pro@gmail.com>
2025-11-20 14:31:12 +0100

    chnagement des noms car oublier de mettre les _

---
+++ ex_01.php
@@ -0,0 +1,8 @@
+<?php
+
+
+    $numbers = ["one", "two", "three", "four", "five"];
+
+        echo "$numbers[3]\n";
+
+    
---
+++ ex_02.php
@@ -0,0 +1,12 @@
+<?php
+
+
+    $person = [
+        "name" => "Romain",
+        "age" => '28',
+        "city" => "Romaiville",
+        
+    ];
+
+        echo $person["name"] . " live in " . $person["city"] . " and is " . $person["age"] . " yo. \n";
+
