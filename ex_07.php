fce6e89fb990abe29dd61d13a106508b786dd979
Moussa Montassar <moussamontassar.pro@gmail.com>
2025-11-21 09:43:15 +0100

    rectification du nombre ou j'ai mit 11 au lieu de 10

--- ex_07.php
+++ ex_07.php
@@ -1,9 +1,10 @@
 <?php
 
-$array = range (1,11);
+$array = range (1,10);
 
       foreach ($array as $nombre){
             if ($nombre % 2 === 0) {
                   echo $nombre . "\n";
             }
       };
+
